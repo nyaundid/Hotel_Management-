3b761f1f0f0249bd8a2f8bf6e997781cb65575be
Biniam <biniamh9@gmail.com>
2018-04-21 07:32:35 -0500

    Rooms class added

---
+++ Hotel Checking System/src/Rooms.java
@@ -0,0 +1,19 @@
+
+public class Rooms {
+	private Rooms[] room = new Rooms[120];
+	private int roomNum;
+	private String type;
+	private String roomStatus;
+	private int numRoomsSingle = 0;
+	private int numRoomDouble = 0;
+	private int numRoomsO =0;
+	public Rooms(){
+	}//no-arg constructor
+
+	public Rooms(int n, double r, String t, String s){
+		roomNum = n;
+		type = t;
+		roomStatus = s;
+		}// Room constructor made to initialise variables.
+
+}
