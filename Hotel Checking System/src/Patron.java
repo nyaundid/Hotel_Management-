1c6f533701202e6325673ba0fe4c2c50f08bbf79
Biniam <biniamh9@gmail.com>
2018-04-21 07:29:38 -0500

    Classes added with preliminary codes

--- Hotel Checking System/src/Patron.java
+++ Hotel Checking System/src/Patron.java
@@ -1,4 +1,32 @@
 
 public class Patron {
+	private Patron[] Patron = new Patron[40];
+	private int numCus;
+	private String name;
+	
 
-}
+	public Patron(){
+	}//no-arg constructor
+
+	public Patron(String n, double p){
+	name = n;
+	numCus ++;
+	}//end constructor
+
+	public String getName(){
+	return name;
+	}
+
+	public Patron[] getPatron(){
+	return Patron;
+	}
+
+
+
+
+	public void printCus(){
+	System.out.printf("Patron name: %s", name);
+	}
+
+	}
+
